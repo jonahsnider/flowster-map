bbf6faac0faba2623905441765d842f7f4018fda
Jonah Snider <jonah@jonahsnider.com>
2021-12-17 06:10:37 -0800

    fix: avoid calling `initializeApp` several times during dev mode

--- src/backend/firebase/firebase.ts
+++ src/backend/firebase/firebase.ts
@@ -2,6 +2,12 @@ import process from 'process';
 
 import * as firebase from 'firebase-admin';
 
-export const app = firebase.initializeApp({
-	credential: firebase.credential.cert(JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY!)),
-});
+export const app =
+	firebase.apps[0] ??
+	firebase.initializeApp({
+		credential: firebase.credential.cert({
+			projectId: 'flowster-map',
+			clientEmail: process.env.GOOGLE_CLIENT_EMAIL!,
+			privateKey: process.env.GOOGLE_PRIVATE_KEY!,
+		}),
+	});
